1bc60c54b07cfdef717c34dfb5d9f64a80144c93
TuncerCatalkaya <Tuncercatalkaya@hotmail.com>
2021-12-04 02:49:45 +0100

    day1

---
+++ day1/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"AdventOfCode2021/filereader"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func main() {
+	challenge1()
+	challenge2()
+}
+
+func challenge1() {
+	lines, err := filereader.ReadLines("day1/input.txt")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	var sweepReport []int64
+
+	for i := range lines {
+		parseInt, err := strconv.ParseInt(lines[i], 10, 64)
+		if err != nil {
+			return
+		}
+		sweepReport = append(sweepReport, parseInt)
+	}
+
+	var counter int64 = 0
+	for i := 0; i < len(sweepReport); i++ {
+		if i+1 > len(sweepReport)-1 {
+			break
+		}
+
+		prevDepth := sweepReport[i]
+		nextDepth := sweepReport[i+1]
+
+		if nextDepth > prevDepth {
+			counter++
+		}
+	}
+	fmt.Println(counter)
+}
+
+func challenge2() {
+	lines, err := filereader.ReadLines("day1/input.txt")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	var sweepReport []int64
+
+	for i := range lines {
+		parseInt, err := strconv.ParseInt(lines[i], 10, 64)
+		if err != nil {
+			return
+		}
+		sweepReport = append(sweepReport, parseInt)
+	}
+
+	var advancedSweepReport []int64
+	for i := 0; i < len(sweepReport); i++ {
+		if i+2 > len(sweepReport)-1 {
+			break
+		}
+
+		advancedSweepReport = append(advancedSweepReport, sweepReport[i]+sweepReport[i+1]+sweepReport[i+2])
+	}
+
+	var counter int64 = 0
+	for i := 0; i < len(advancedSweepReport); i++ {
+		if i+1 > len(advancedSweepReport)-1 {
+			break
+		}
+
+		prevDepth := advancedSweepReport[i]
+		nextDepth := advancedSweepReport[i+1]
+
+		if nextDepth > prevDepth {
+			counter++
+		}
+	}
+	fmt.Println(counter)
+}
---
+++ filereader/filereader.go
@@ -0,0 +1,26 @@
+package filereader
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			return
+		}
+	}(file)
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
